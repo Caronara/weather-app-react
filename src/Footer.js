672c2c828b7f320bf31e99dd3252d409df2a022a
Caronara <97363450+Caronara@users.noreply.github.com>
2022-04-10 15:28:24 +0200

    Changed GitHub link

--- src/Footer.js
+++ src/Footer.js
@@ -3,10 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="Footer">
-      <a
-        href="https://github.com/Caronara/weather-app-folder"
-        className="github"
-      >
+      <a href="https://github.com/Caronara" className="github">
         Open-source code
       </a>
       &nbsp; by Caroline
